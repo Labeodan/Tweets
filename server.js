46f6de12129fc859b7d5a1e8709867a416f09b84
Ojiromayo <labeodanojiromayo@gmail.com>
2024-12-17 00:22:53 -0500

    I implemented log rotations

--- server.js
+++ server.js
@@ -5,18 +5,32 @@ require("dotenv").config()
 const app = express()
 const authRouter = require("./controllers/users")
 const tweetRouter = require("./controllers/tweets")
+const rfs = require("rotating-file-stream");
+const path = require("path")
+const rateLimit = require("express-rate-limit")
 
 
+// setting up log rotation
+const logStream = rfs.createStream("access.log", {
+    interval: "1d", // Rotate daily
+    path: path.join(__dirname, "logs"), // Directory to save logs
+    maxFiles: 7, // Keep logs for the last 7 days
+});
 
 
 //! Middleware
-app.use(morgan("dev"))
+app.use(morgan("dev", { stream: process.stdout })); // Log to terminal
+app.use(morgan("combined", { stream: logStream }));
 app.use(express.json())
+
+
+// Setting up Rate Limiting
 
 
 // !Routes
 app.use("/auth", authRouter)
 app.use("/tweets", tweetRouter)
+
 
   
 
